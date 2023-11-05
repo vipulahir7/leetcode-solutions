845fc0d02a1645ec1edf3a2056b44bc03f58832f
ahir vipul <114806840+ahirvipul7@users.noreply.github.com>
2023-11-06 00:49:59 +0530

    Time: 3 ms (90.16%), Space: 13.4 MB (59.12%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
         
-    int f(vector<int>& prices ,int i,int n,bool ableToBuy,vector<vector<int>> &dp){
+//     int f(vector<int>& prices ,int i,int n,bool ableToBuy,vector<vector<int>> &dp){
         
-        if(i>=n){
-            return 0;
-        }
+//         if(i>=n){
+//             return 0;
+//         }
         
-        if(dp[i][ableToBuy]!=-1){
-            return dp[i][ableToBuy];
-        }
+//         if(dp[i][ableToBuy]!=-1){
+//             return dp[i][ableToBuy];
+//         }
         
-        if(ableToBuy){
-            return dp[i][ableToBuy]=max(-prices[i] + f(prices,i+1,n,false,dp) , f(prices,i+1,n,true,dp));
-        }
-        return dp[i][ableToBuy]=max(prices[i] + f(prices,i+1,n,true,dp) , f(prices,i+1,n,false,dp));
+//         if(ableToBuy){
+//             return dp[i][ableToBuy]=max(-prices[i] + f(prices,i+1,n,false,dp) , f(prices,i+1,n,true,dp));
+//         }
+//         return dp[i][ableToBuy]=max(prices[i] + f(prices,i+1,n,true,dp) , f(prices,i+1,n,false,dp));
         
-    }
+//     }
     
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
         
-        return f(prices,0,n,true,dp);
+        vector<int> prev(2,0),cur(2);
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                if(j){
+                    cur[j]=max(prev[0]-prices[i],prev[1]);
+                }   
+                else{
+                    cur[j]=max(prices[i]+prev[1],prev[0]);
+                }
+            }
+            prev=cur;
+        }
+        
+        return prev[1];
     }
 };
