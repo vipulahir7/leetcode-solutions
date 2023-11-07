806853ecbb62b3c0d2b2bbbdf8711713e823fa44
ahir vipul <114806840+ahirvipul7@users.noreply.github.com>
2023-11-08 00:41:43 +0530

    Time: 1678 ms (10.56%), Space: 422.6 MB (8.75%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,50 +1,31 @@
 class Solution {
 public:
 
-    // int f(string s,string t,int n,int m,vector<vector<int>> &dp){
+    int f(string s,string t,int n,int m,vector<vector<int>> &dp){
 
-    //     if(m<0){
-    //         return 1;
-    //     }
-    //     if(n<0){
-    //         return 0;
-    //     }
+        if(m<0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
 
-    //     if(dp[n][m]!=-1){
-    //         return dp[n][m];
-    //     }
+        if(dp[n][m]!=-1){
+            return dp[n][m];
+        }
 
-    //     if(s[n]==t[m]){
-    //         return dp[n][m]=f(s,t,n-1,m-1,dp)+f(s,t,n-1,m,dp);
-    //     }
+        if(s[n]==t[m]){
+            return dp[n][m]=f(s,t,n-1,m-1,dp)+f(s,t,n-1,m,dp);
+        }
 
-    //     return dp[n][m]=f(s,t,n-1,m,dp);
-    // }
+        return dp[n][m]=f(s,t,n-1,m,dp);
+    }
 
     int numDistinct(string s, string t) {
 
         int n=s.length(),m=t.length();
-        vector<double> prev(m+1,0),cur(m+1);
-        prev[0]=1;
+        vector<vector<int>> dp(n,vector<int>(m,-1));
 
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                if(j==0){
-                    cur[j]=1;
-                }
-
-                if(i && j){
-                    if(s[i-1]==t[j-1]){
-                        cur[j]=prev[j-1]+prev[j];
-                    }
-                    else{
-                        cur[j]=prev[j];
-                    }
-                }
-            }
-            prev=cur;
-        }
-
-        return (int)prev[m];
+        return f(s,t,n-1,m-1,dp);
     }
 };
