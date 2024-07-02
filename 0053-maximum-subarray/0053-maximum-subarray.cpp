7b9637c4ad818dc6b1fe3fd822b8dc8d51049127
ahir vipul <114806840+vipulahir7@users.noreply.github.com>
2024-07-02 22:22:05 +0530

    Time: 71 ms (83.36%), Space: 70.1 MB (92.58%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,9 +3,8 @@ public:
     int maxSubArray(vector<int>& nums) {
         int ans = -1000001,current = 0;
         for(auto num : nums) {
-            current += num;
+            current = max(num, num + current);
             ans = max(ans, current);
-            if(current < 0) current  = 0;
         }
         return ans;
     }
