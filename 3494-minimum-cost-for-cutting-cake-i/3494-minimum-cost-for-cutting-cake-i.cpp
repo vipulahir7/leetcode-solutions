e8da33f544198ca3449a9276d8c18366523fd118
ahir vipul <114806840+vipulahir7@users.noreply.github.com>
2024-07-15 12:23:58 +0530

    Time: 0 ms (100%), Space: 30.8 MB (50%) - LeetHub

---
+++ 3494-minimum-cost-for-cutting-cake-i/3494-minimum-cost-for-cutting-cake-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minimumCost(int m, int n, vector<int>& a, vector<int>& b) {
+        int h=1, v=1;
+        int i=0,j=0;
+        int ans = 0;
+        sort(a.rbegin(), a.rend());
+        sort(b.rbegin(), b.rend());
+        while(i<m-1 && j<n-1){
+            if(a[i]>=b[j]){
+                ans += v*a[i];
+                i++;
+                h++;
+            }
+            else if(b[j] > a[i]){
+                ans += h*b[j];
+                v++;
+                j++;
+            }
+        }
+        while(i<m-1){
+            ans += v*a[i];
+            i++;
+        }
+        while(j< n-1){
+            ans += h*b[j];
+            j++;
+        }
+        return ans;
+    }
+    
+};
