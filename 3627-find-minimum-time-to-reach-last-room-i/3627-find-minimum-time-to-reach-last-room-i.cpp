81235f3bb60e307a3526b862b83b421edca4bfd8
ahir vipul <114806840+vipulahir7@users.noreply.github.com>
2024-11-04 14:35:11 +0530

    Time: 66 ms (100%), Space: 39.5 MB (100%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -3,34 +3,34 @@ public:
 
     bool validate(int i, int j, int n, int m){
         return (i>=0 && i<n && j<m && j>=0);
-    }
-
-    void solve(int i, int j, int time,int n, int m, vector<vector<int>> & minTime, vector<vector<int>>& moveTime){
-        
-        if(validate(i+1, j, n, m) && max(moveTime[i+1][j], time)+1 < minTime[i+1][j]){
-            minTime[i+1][j] = max(moveTime[i+1][j], time)+1;
-            solve(i+1, j, max(moveTime[i+1][j], time)+1, n, m, minTime, moveTime);
-        }
-        if(validate(i-1, j, n, m) && max(moveTime[i-1][j], time)+1 < minTime[i-1][j]){
-            minTime[i-1][j] = max(moveTime[i-1][j], time)+1;
-            solve(i-1, j, max(moveTime[i-1][j], time)+1, n, m, minTime, moveTime);
-        }
-        if(validate(i, j+1, n, m) && max(moveTime[i][j+1], time)+1 < minTime[i][j+1]){
-            minTime[i][j+1] = max(moveTime[i][j+1], time)+1;
-            solve(i, j+1, max(moveTime[i][j+1], time)+1, n, m, minTime, moveTime);
-        }
-        if(validate(i, j-1, n, m) && max(moveTime[i][j-1], time)+1 < minTime[i][j-1]){
-            minTime[i][j-1] = max(moveTime[i][j-1], time)+1;
-            solve(i, j-1, max(moveTime[i][j-1], time)+1, n, m, minTime, moveTime);
-        }
-    }
+    }   
 
     int minTimeToReach(vector<vector<int>>& moveTime) {
         int n = moveTime.size();
         int m = moveTime[0].size();
-        vector<vector<int>> minTime(n, vector<int>(m, INT_MAX));
 
-        solve(0, 0, 0,n,m, minTime, moveTime);
-        return minTime[n-1][m-1];
+        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<>> heap;
+        heap.push({0, 0, 0});
+        vector<vector<int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        set<pair<int, int>> visited;
+
+        while(!heap.empty()){
+            auto [time, i, j] = heap.top();
+            heap.pop();
+
+            if(i==n-1 && j == m-1){
+                return time;
+            }
+
+            for(auto d : dir){
+                int row = i + d[0], col = j + d[1];
+                if(validate(row, col, n, m) && !visited.count({row, col})){
+                    heap.push({max(time, moveTime[row][col]) + 1, row, col});
+                    visited.insert({row, col});
+                }
+            }
+        }
+            return -1;
+
     }
 };
